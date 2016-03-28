40071ed57048098a68dff06f6327c91778b511a0
simoncarron <simon.carron@gmail.com>
2016-03-28 10:12:31 +0200

    Test

--- run.py
+++ run.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
 
-#TEST DESCRIPTION
-
-print "sdsdsdsd"
 app.run(
 
     	debug=config.DEBUG,
